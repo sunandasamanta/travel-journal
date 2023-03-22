f134c118ce4e8da40eebb4bf828ad80bff8dac90
sunandasamanta <samantasunanda724@gmail.com>
2023-03-22 14:35:59 +0530

    final card design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,14 @@ import { MdLocationOn } from 'react-icons/md'
 export default function Card({ data }) {
     console.log(data);
     return (
-        <div className="flex gap-4 p-4" >
-            <img className="rounded-xl w-[225px] aspect-[3/4] object-cover" src={data.image} alt="" />
+        <div className="flex sm:flex-wrap sm:justify-center gap-4 p-4" >
+            <img className="rounded-xl max-w-[250px] aspect-[3/4] object-cover" src={data.image} alt="" />
             <div className="grid place-content-center">
                 <p className="flex text-xl items-center">
                     <MdLocationOn className="text-[#F55A5A] text-xl" />
-                    <span className="tracking-widest font-norma px-2 text-[#2B283A]">JAPAN</span>
+                    <span className="tracking-widest font-normal px-2 text-[#2B283A]">
+                        {data.country}
+                    </span>
                     <a href={data.googleMap} className="underline px-4 text-gray-400">View on Google Maps</a>
                 </p>
                 <h2 className="font-bold text-5xl my-4">
