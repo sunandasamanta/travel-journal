9e5f828e1c31935c1c669a1f5b71006a0b0f769e
sunandasamanta <samantasunanda724@gmail.com>
2023-03-22 14:35:51 +0530

    site data

--- src/Data.js
+++ src/Data.js
@@ -9,5 +9,27 @@ export default [
             period: "12 Jan, 2021 - 24 Jan, 2021",
             description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
         }
+    },
+    {
+        id: 2,
+        info: {
+            image: "https://source.unsplash.com/JmuyB_LibRo",
+            country: "AUSTRALIA",
+            place: "Sydney Opera House",
+            googleMap: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.213108,17z/data=!3m1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m14!1m7!3m6!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!2sSydney+Opera+House!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0",
+            period: "27 May, 2021 - 8 Jun, 2021",
+            description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
+        }
+    },
+    {
+        id: 3,
+        info: {
+            image: "https://source.unsplash.com/3PeSjpLVtLg",
+            country: "NORWAY",
+            place: "Geirangerfjord",
+            googleMap: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.213108,17z/data=!3m1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m14!1m7!3m6!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!2sSydney+Opera+House!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0",
+            period: "01 Oct, 2021 - 18 Nov, 2021",
+            description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
+        }
     }
 ]
